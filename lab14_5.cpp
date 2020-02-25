ecc61f1ddcc8f28e7122e773041b56cccb255dca
sathayu2544 <59278037+sathayu2544@users.noreply.github.com>
2020-02-25 17:01:06 +0700

    Update lab14_5.cpp

--- lab14_5.cpp
+++ lab14_5.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +12,24 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+//Write definition of shuffle() here 
+void shuffle(int *a,int *b, int *c, int *d){
+int p[] = {*a,*b,*c,*d},y;
+for (int i = 0;i<4;i++){
+	int x = rand()%4;
+	y=p[i];
+	p[i]=p[x];
+	p[x]=y;
+}
+*a=p[0];
+*b=p[1];
+*c=p[2];
+*d=p[3];
+}
